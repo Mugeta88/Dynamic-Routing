42f363187c19df09943415a0eeeb7c488efffa81
Your Name <mugeta88@gmail.com>
2026-01-11 20:25:19 -0500

    added some data for the blog post

---
+++ src/lib/posts.js
@@ -0,0 +1,22 @@
+const posts = [
+  {
+    id: 1,
+    slug: "my-first-post",
+    title: "My First Week",
+    content: "My first week learning HTML and CSS."
+  },
+  {
+    id: 2,
+    slug: "react-router-basics",
+    title: "My Second Week",
+    content: "My second week learning JavaScript."
+  },
+  {
+    id: 3,
+    slug: "context-api-auth",
+    title: "My Third Week",
+    content: "My third week learning React.js."
+  }
+];
+
+export default posts;
